9491c20dca69bb1f305f68b03dfb7df169e227fb
CODEWITHRISHU2005 <rg2822046@gmail.com>
2025-12-19 15:38:25 +0530

    update @19-12-25

--- Video-Streaming-App/src/main/java/com/CodeWithRishu/Video_Streaming_App/VideoStreamingAppApplication.java
+++ Video-Streaming-App/src/main/java/com/CodeWithRishu/Video_Streaming_App/VideoStreamingAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableAsync
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://video-streaming-app-lac.vercel.app/")
 public class VideoStreamingAppApplication {
 
 	public static void main(String[] args) {
