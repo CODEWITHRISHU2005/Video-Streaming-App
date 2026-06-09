bebfe21c7e8175ba54b6103d4e0e04d88a4035d1
CODEWITHRISHU2005 <rg2822046@gmail.com>
2026-06-09 11:35:59 +0530

    update @09-06-26

--- Video-Streaming-App/src/main/java/com/CodeWithRishu/Video_Streaming_App/VideoStreamingAppApplication.java
+++ Video-Streaming-App/src/main/java/com/CodeWithRishu/Video_Streaming_App/VideoStreamingAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin(origins = {"${APP_FRONTEND_URL}"})
+@CrossOrigin(origins = "${app.frontend.url}")
 public class VideoStreamingAppApplication {
 
 	public static void main(String[] args) {
